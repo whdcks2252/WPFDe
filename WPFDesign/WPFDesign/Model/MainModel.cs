31b8a11d46cf00604c66bf2b2bd54bc14cb8cc7b
wonjongchan <whdcks4569@gmail.com>
2023-10-06 17:50:51 +0900

    test

---
+++ WPFDesign/WPFDesign/Model/MainModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDesign.Model
+{
+    
+    public class MenuItems
+    {
+        public string MenuName { get; set; }
+        public string MenuImage { get; set; }
+
+    }
+    public class HomeItems
+    {
+        public string HomeName { get; set; }
+        public string HomeImage { get; set; }
+
+    }
+    public class PCItems
+    {
+        public string PCName { get; set; }
+        public string PCImage { get; set; }
+
+    }
+    public class DesktopItems
+    {
+        public string DesktopName { get; set; }
+        public string DesktopImage { get; set; }
+
+    }
+    public class DocumentItems
+    {
+        public string DocumentName { get; set; }
+        public string DocumentImage { get; set; }
+
+    }
+    public class DownloadItems
+    {
+        public string DownloadName { get; set; }
+        public string DownloadImage { get; set; }
+
+    }
+    public class PictureItems
+    {
+        public string PictureName { get; set; }
+        public string PictureImage { get; set; }
+
+    }
+    public class MusicItems
+    {
+        public string MusicName { get; set; }
+        public string MusicImage { get; set; }
+
+    }
+    public class MovieItems
+    {
+        public string MovieName { get; set; }
+        public string MovieImage { get; set; }
+
+    }
+    public class TrashItems
+    {
+        public string TrashName { get; set; }
+        public string TrashImage { get; set; }
+
+    }
+}
